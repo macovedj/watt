19ad729f193734d34b25b0b6c350dcc6133c6d49
macovedj <djmacovei@gmail.com>
2026-01-08 15:42:12 -0600

    Fix reference type opcodes to match working backup (0xc0-0xc2 not 0xd0-0xd2)

--- src/binary.rs
+++ src/binary.rs
@@ -554,13 +554,13 @@ impl<R: Read> Decoder<R> {
             }),
 
             // Reference types proposal instructions
-            0xd0 => {
+            0xc0 => {
                 RefNull
             }
-            0xd1 => {
+            0xc1 => {
                 RefIsNull
             }
-            0xd2 => {
+            0xc2 => {
                 let func_index = self.read_index()?;
                 RefFunc(func_index)
             }
